c5c2208be188ffed6ec2f5993e334716218b9766
activiteOCR <julien.terrier3@wanadoo.fr>
2024-03-23 15:59:12 +0100

    AU - Week5 - Hardhat local games

--- scripts/win.js
+++ scripts/win.js
@@ -1,23 +1,27 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0";
+const contractName = "Game5";
 
 async function main() {
-    // attach to the game
-    const game = await hre.ethers.getContractAt(contractName, gameAddr);
+  // attach to the game
+  const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-    // do whatever you need to do to win the game here:
-    const tx = await game.win();
+  // do whatever you need to do to win the game here:
+  const tx1 = await game.giveMeAllowance(10000);
+  await tx1.wait();
+  const tx2 = await game.mint(10000);
+  await tx2.wait();
+  const tx3 = await game.win();
 
-    // did you win? Check the transaction receipt!
-    // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
-    console.log(receipt);
+  // did you win? Check the transaction receipt!
+  // if you did, it will be in both the logs and events array
+  const receipt = await tx3.wait();
+  console.log(receipt);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
